dae70576770b195a90a1921db58e095147d111a0
Khizar Qureshi <khizarq313@gmail.com>
2022-09-04 01:00:22 +0530

    update 4 august,2022.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const loginSuccess = document.querySelector(".loginSuccess");
 
 const loginFailed = document.querySelector(".loginFailed");
 
-const overlay = document.querySelector(".overlay")
+const overlay = document.querySelector(".overlay");
 
 const successBtn = document.querySelector(".success-btn");
 
@@ -17,37 +17,36 @@ const failBtn = document.querySelector(".fail-btn");
 const loginBox = document.querySelector(".login-box");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-})
+  e.preventDefault();
+});
 
 function checkUserInput(userInput) {
-    const userName = userInput[0].value;
-    const userPassword = userInput[1].value;
-    if (userName === "khizarq313" &&
-    userPassword === "ronaldo07" ) {
+  const userName = userInput[0].value;
+  const userPassword = userInput[1].value;
+  if (userName === "khizarq313" && userPassword === "ronaldo07") {
     loginSuccess.classList.add("visible");
-} else {
+  } else {
     loginFailed.classList.add("visible");
-}
+  }
 }
 
 function getUserInputFn() {
-    loginBox.classList.add("login-box-invisible")
-    overlay.classList.add("visible");
-    checkUserInput(userInput);
+  loginBox.classList.add("login-box-invisible");
+  overlay.classList.add("visible");
+  checkUserInput(userInput);
 }
 
 function closeResultWindow() {
-    loginBox.classList.remove("login-box-invisible")
-    overlay.classList.remove("visible")
-    loginSuccess.classList.remove("visible");
-    loginFailed.classList.remove("visible");
+  loginBox.classList.remove("login-box-invisible");
+  overlay.classList.remove("visible");
+  loginSuccess.classList.remove("visible");
+  loginFailed.classList.remove("visible");
 }
 
-getDataBtn.addEventListener("click",getUserInputFn);
+getDataBtn.addEventListener("click", getUserInputFn);
 
-successBtn.addEventListener("click",closeResultWindow);
+successBtn.addEventListener("click", closeResultWindow);
 
-failBtn.addEventListener("click",closeResultWindow);
+failBtn.addEventListener("click", closeResultWindow);
 
-overlay.addEventListener("click",closeResultWindow)
+overlay.addEventListener("click", closeResultWindow);
