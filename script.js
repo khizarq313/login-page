d912ad0a948acada3702de766c5b3fe2c02286af
Khizar Qureshi <khizarq313@gmail.com>
2022-09-04 00:58:39 +0530

    update on august 3,2022.

--- script.js
+++ script.js
@@ -8,19 +8,46 @@ const loginSuccess = document.querySelector(".loginSuccess");
 
 const loginFailed = document.querySelector(".loginFailed");
 
+const overlay = document.querySelector(".overlay")
+
+const successBtn = document.querySelector(".success-btn");
+
+const failBtn = document.querySelector(".fail-btn");
+
+const loginBox = document.querySelector(".login-box");
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 })
 
-function getUserInputFn() {
+function checkUserInput(userInput) {
     const userName = userInput[0].value;
     const userPassword = userInput[1].value;
     if (userName === "khizarq313" &&
-        userPassword === "ronaldo07" ) {
-        loginSuccess.classList.toggle("resultVisible");
-    } else {
-        loginFailed.classList.toggle("resultVisible");
-    }
+    userPassword === "ronaldo07" ) {
+    loginSuccess.classList.add("visible");
+} else {
+    loginFailed.classList.add("visible");
+}
+}
+
+function getUserInputFn() {
+    loginBox.classList.add("login-box-invisible")
+    overlay.classList.add("visible");
+    checkUserInput(userInput);
+}
+
+function closeResultWindow() {
+    loginBox.classList.remove("login-box-invisible")
+    overlay.classList.remove("visible")
+    loginSuccess.classList.remove("visible");
+    loginFailed.classList.remove("visible");
 }
 
 getDataBtn.addEventListener("click",getUserInputFn);
+
+successBtn.addEventListener("click",closeResultWindow);
+
+failBtn.addEventListener("click",closeResultWindow);
+
+overlay.addEventListener("click",closeResultWindow)
